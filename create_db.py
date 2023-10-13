19fac770d445dd08940c9423688434c6d96b2638
KilonzoJames <jay.kilonzo@gmail.com>
2023-10-13 17:13:01 +0300

    Modified env to pipfile

--- create_db.py
+++ create_db.py
@@ -3,9 +3,6 @@ from project import db, create_app
 # Create the Flask app instance
 app = create_app()
 
-# Initialize the app with the Flask-SQLAlchemy extension
-db.init_app(app)
-
 # Create the database tables
 with app.app_context():
     db.create_all()
